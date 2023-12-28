571a18085bdc1dd123ffd14281e8eb79b7356df5
SistemaMedicoMX <155038495+SistemaMedicoMX@users.noreply.github.com>
2023-12-27 21:07:17 -0600

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,1 +1,14 @@
+<?php
 
+$servidor= "localhost"; 
+$BaseDeDatos="appmedica";
+$usuario="root";
+$contrasenia= "";
+try {
+    $conexion= new PDO("mysql:host:$servidor;dbname:$BaseDeDatos", $usuario,$contrasenia);
+}catch(Exception $ex){
+    echo $ex->getMessage();
+}
+
+
+?>
